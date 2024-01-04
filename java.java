f222b2a52f1b48763d23b9a37d7ceb6ac65c1f2e
Lisenok07 <150365820+Lisenok07@users.noreply.github.com>
2024-01-04 16:01:41 +0100

    Create java.java

---
+++ java.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int variable = 0;
+	    variable++;
+	    System.out.println(variable);
+	    char b = 'a';
+	    System.out.println(b);
+	    String c = "Phrase: ";
+	    String c1 = "I love java";
+	    System.out.println (c + c1);
+	    Scanner scan = new Scanner (System.in);
+	    int number1 = scan.nextInt();
+	    int number2 = scan.nextInt();
+	    System.out.println(number1+number2);
+	    
+	}
+}
